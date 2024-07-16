d78d16e65f94009b3e1f6bf8a34f02d7365a7b96
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-07-17 00:33:38 +0900

    section#6.2 Deps
    state가 변하면 모든게 리 렌더링되는 걸 방지하고 특정 state가 변할때만 어떤 코드를 실행시키게 만들고 싶다면 useEffect의 두번째 argument 자리에 한개 이상의 state 변수명을 넣어주면 됌

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
 
   const onClick = () => {
     setCounter((prev) => prev + 1);
   };
 
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
   const iRunOnlyOnce = () => {
     console.log("I run only once.");
   };
+
   useEffect(iRunOnlyOnce, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run only when keyword changes");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    if (counter !== 0) {
+      console.log("I run only when counter changes");
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run only when keyword and counter changes");
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+        onChange={onChange}
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>Click Me</button>
     </div>
