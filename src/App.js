bd48d1d9a56f3cf2ccd056cac95744dad13f5dbf
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-07-16 17:40:37 +0900

    section#6.1 useEfftect
    특정코드를 state가 변해도 초기에 한번만 실행되게 해주는 기능으로  useEfftect라는 걸 쓸 수 있음. useEffect를 사용했는데 초기에 2번 실행되는 현상이 발생하여 알아보니 index.js에서  <React.StrictMode> 로 감싸져있는 부분을 지우면 된다고함, 이것이 무슨 기능을 하는지 추후 알아볼 것.
    
    useEffect의 두번째 argument는 대체 무엇인지에 대해서는 다음 강의에서 다룰 예정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -6,6 +6,11 @@ function App() {
   const onClick = () => {
     setCounter((prev) => prev + 1);
   };
+
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>
