f888d13b2a318233249c71af8a51edc35a8715d3
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-07-23 20:06:36 +0900

    section#7.2 Coin Tracker
    api로 부터 코인들에 대한 정보를 받아와서 리스트로 그리는 실습 완료. 과제인 가진 예산으로 얼만큼 많은 코인을 살 수 있는지 자동 계산해주는 기능도 구현완료.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [money, setMoney] = useState(0);
+  const [coinValue, setCoinValue] = useState("");
+  const [affordableCoinCnt, setAffordableCoinCnt] = useState(0);
+
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
+      response.json().then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+    );
+  }, []);
+
   const onChange = (event) => {
-    setToDo(event.target.value);
+    setMoney(event.target.value);
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+
+  const onChangeSelect = (event) => {
+    setCoinValue(event.target.value);
+    console.log(coinValue);
+    setAffordableCoinCnt(money / parseFloat(coinValue));
   };
+
   return (
     <div>
-      <h1>My TO Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          type="text"
-          value={toDo}
-          placeholder="Write your to do"
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins ({coins.length})</h1>
+      <span>I have</span>
+      <input onChange={onChange}></input> <span>USD Dollars</span>
+      <br></br>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onChangeSelect} value={coinValue}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <br></br>
+      <h1>You can buy {affordableCoinCnt} coins</h1>
     </div>
   );
 }
