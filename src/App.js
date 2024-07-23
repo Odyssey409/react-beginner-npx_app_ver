aeb8d9e6362bfed9a03b6e96291ca49c8cc8ba24
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-07-23 18:38:21 +0900

    section#7.0 To Do List part One
    배열에 새로운 원소를 추가하고 싶을 때 기존 자바스크립트에서는 push 함수를 썼지만 state에 저장하기 위해서는 다른 방식을 써야함. 새로운 빈 배열에 원하는 값과 기존 배열에 저장돼 있던 값을 합치고 싶으면 [새로운 값, 기존 배열]을 한다고 생각하는데, 이러면 이중 배열이 되고 이를 해결하려면 [새로운 값,...기존 배열] 하면 배열안 값만 빠져나와서 새로운 배열 만들어줌. ... 이게 그 역할임

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  function destroyedFn() {
-    console.log("destroyed :(");
-  }
-  function createdFn() {
-    console.log("created :)");
-    return destroyedFn;
-  }
-  useEffect(createdFn, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-
-  const onClick = () => {
-    setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
   };
-
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My TO Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do"
+        ></input>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
