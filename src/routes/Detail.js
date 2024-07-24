89a70bf3d8fd5004f5289064c7168497830a69c8
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-07-24 22:01:20 +0900

    section#7.6 Parameters
    영화 타이틀을 클릭하면 아이디를 받아와서 매개변수로 활용하여 url을 변화시켜 영화 상세 페이지로 라우팅 시켜주고 그 안에서 그 영화에 대한 상세정보를 보여줄 수 있는 기능 구현 완료

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovie(json.data.movie);
+      setLoading(false);
+      console.log(movie);
+    };
+    getMovie();
+  }, [id, movie]);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <p>{movie.description_full}</p>
+          <ul>
+            {movie.genres?.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+          <span>rating: {movie.rating}/10</span>
+          <br></br>
+          <span>runtime: {movie.runtime} mins</span>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
